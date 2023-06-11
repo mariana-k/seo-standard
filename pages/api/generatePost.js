e6eda079c013bcc6898996587f5a0ff5ccb01f63
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2023-06-11 15:51:48 +0300

    Changed name added site url

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,38 +1,38 @@
-import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
-import { Configuration, OpenAIApi } from 'openai';
-import clientPromise from '../../lib/mongodb';
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { Configuration, OpenAIApi } from 'openai'
+import clientPromise from '../../lib/mongodb'
 
 export default withApiAuthRequired(async function handler(req, res) {
-  const { user } = await getSession(req, res);
-  const client = await clientPromise;
-  const db = client.db('SEOStandard');
-  const userProfile = await db.collection('users').findOne({
-    auth0Id: user.sub,
-  });
+	const { user } = await getSession(req, res)
+	const client = await clientPromise
+	const db = client.db('SEOStandard')
+	const userProfile = await db.collection('users').findOne({
+		auth0Id: user.sub,
+	})
 
-  if (!userProfile?.availableTokens) {
-    res.status(403);
-    return;
-  }
+	if (!userProfile?.availableTokens) {
+		res.status(403)
+		return
+	}
 
-  const config = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(config);
+	const config = new Configuration({
+		apiKey: process.env.OPENAI_API_KEY,
+	})
+	const openai = new OpenAIApi(config)
 
-  const { topic, keywords } = req.body;
+	const { topic, keywords } = req.body
 
-  if (!topic || !keywords) {
-    res.status(422);
-    return;
-  }
+	if (!topic || !keywords) {
+		res.status(422)
+		return
+	}
 
-  if (topic.length > 80 || keywords.length > 80) {
-    res.status(422);
-    return;
-  }
+	if (topic.length > 80 || keywords.length > 80) {
+		res.status(422)
+		return
+	}
 
-  /*const response = await openai.createCompletion({
+	/*const response = await openai.createCompletion({
     model: 'text-davinci-003',
     temperature: 0,
     max_tokens: 3600,
@@ -47,85 +47,84 @@ export default withApiAuthRequired(async function handler(req, res) {
     }`,
   });*/
 
-  const postContentResult = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'system',
-        content: 'You are a blog post generator.',
-      },
-      {
-        role: 'user',
-        content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+	const postContentResult = await openai.createChatCompletion({
+		model: 'gpt-3.5-turbo',
+		messages: [
+			{
+				role: 'system',
+				content: 'You are a blog post generator.',
+			},
+			{
+				role: 'user',
+				content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
       The response should be formatted in SEO-friendly HTML, 
       limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
-      },
-    ],
-    temperature: 0,
-  });
+			},
+		],
+		temperature: 0,
+	})
 
-  const postContent = postContentResult.data.choices[0]?.message.content;
+	const postContent = postContentResult.data.choices[0]?.message.content
 
-  const titleResult = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'system',
-        content: 'You are a blog post generator.',
-      },
-      {
-        role: 'user',
-        content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+	const titleResult = await openai.createChatCompletion({
+		model: 'gpt-3.5-turbo',
+		messages: [
+			{
+				role: 'system',
+				content: 'You are a blog post generator.',
+			},
+			{
+				role: 'user',
+				content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
       The response should be formatted in SEO-friendly HTML, 
       limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
-      },
-      {
-        role: 'assistant',
-        content: postContent,
-      },
-      {
-        role: 'user',
-        content: 'Generate appropriate title tag text for the above blog post',
-      },
-    ],
-    temperature: 0,
-  });
+			},
+			{
+				role: 'assistant',
+				content: postContent,
+			},
+			{
+				role: 'user',
+				content: 'Generate appropriate title tag text for the above blog post',
+			},
+		],
+		temperature: 0,
+	})
 
-  const metaDescriptionResult = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'system',
-        content: 'You are a blog post generator.',
-      },
-      {
-        role: 'user',
-        content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+	const metaDescriptionResult = await openai.createChatCompletion({
+		model: 'gpt-3.5-turbo',
+		messages: [
+			{
+				role: 'system',
+				content: 'You are a blog post generator.',
+			},
+			{
+				role: 'user',
+				content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
       The response should be formatted in SEO-friendly HTML, 
       limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
-      },
-      {
-        role: 'assistant',
-        content: postContent,
-      },
-      {
-        role: 'user',
-        content:
-          'Generate SEO-friendly meta description content for the above blog post',
-      },
-    ],
-    temperature: 0,
-  });
+			},
+			{
+				role: 'assistant',
+				content: postContent,
+			},
+			{
+				role: 'user',
+				content:
+					'Generate SEO-friendly meta description content for the above blog post',
+			},
+		],
+		temperature: 0,
+	})
 
-  const title = titleResult.data.choices[0]?.message.content;
-  const metaDescription =
-    metaDescriptionResult.data.choices[0]?.message.content;
+	const title = titleResult.data.choices[0]?.message.content
+	const metaDescription = metaDescriptionResult.data.choices[0]?.message.content
 
-  console.log('POST CONTENT: ', postContent);
-  console.log('TITLE: ', title);
-  console.log('META DESCRIPTION: ', metaDescription);
+	console.log('POST CONTENT: ', postContent)
+	console.log('TITLE: ', title)
+	console.log('META DESCRIPTION: ', metaDescription)
 
-  /*await db.collection('users').updateOne(
+	/*await db.collection('users').updateOne(
   {
     auth0Id: user.sub,
   },
@@ -136,17 +135,17 @@ export default withApiAuthRequired(async function handler(req, res) {
   }
 );*/
 
-  const post = await db.collection('posts').insertOne({
-    postContent: postContent || '',
-    title: title || '',
-    metaDescription: metaDescription || '',
-    topic,
-    keywords,
-    userId: userProfile._id,
-    created: new Date(),
-  });
+	const post = await db.collection('posts').insertOne({
+		postContent: postContent || '',
+		title: title || '',
+		metaDescription: metaDescription || '',
+		topic,
+		keywords,
+		userId: userProfile._id,
+		created: new Date(),
+	})
 
-  res.status(200).json({
-    postId: post.insertedId,
-  });
-});
+	res.status(200).json({
+		postId: post.insertedId,
+	})
+})
